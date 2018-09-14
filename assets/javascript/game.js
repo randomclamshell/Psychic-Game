c48747efc3268f549c9fd6f96b1b3c324116f3b6
KI <iqbal.mohammadk@gmail.com>
2018-09-14 11:58:29 -0400

    initial files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,9 @@
 
 //create an array for the letters
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-console.log(letters);
+
 
 //variables to keep score
 var gameScore
@@ -23,44 +23,41 @@ var guesses = 10;
 //print letter typed
 
 
-
 //variables to hold html elements/the text that stays on the screen
-var displayHTML = document.getElementById ("display");
+var displayHTML = document.getElementById("display");
 
 
 //reset the game
 function resetGame() {
   var gameScore = 0;
-var losses = 0;
-var wins = 0;
-var guesses = 10;
+  var losses = 0;
+  var wins = 0;
+  var guesses = 10;
 }
 
 //print the score to the page
-printScore(gameScore, losses, wins, guesses);
-print(keyPressed);
 
 //what is going to happen onkeyup
-document.onkeyup = function(event) {}
+document.onkeyup = function (event) {
 
-//get the value of the key that's pressed
-var keyPressed = event.key;
+  //get the value of the key that's pressed
+  var keyPressed = event.key;
 
-// variable statements
-if (keyPressed !== letters) {
+  // variable statements
+  if (keyPressed !== letters) {
 
-  losses++;
-  guesses --;
-}
+    losses++;
+    guesses--;
+  }
 
-if (keyPressed === letters) {
+  if (keyPressed === letters) {
 
-  wins++;
-  guesses --;
-}
+    wins++;
+    guesses--;
+  }
 
-if (guesses === 0) {
-  messageHTML.textContent = "Out of guesses! :("
-}
-//stop everything from running at game over
-return false;
+  if (guesses === 0) 
+    messageHTML.textContent = "Out of guesses! :("
+    //stop everything from running at game over
+    //return false;
+  }
